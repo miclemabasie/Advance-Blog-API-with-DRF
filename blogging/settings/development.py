080200d7f7577c2321d49bc88d546a42a6f273bd
MICLEM <miclemabasie3@gmail.com>
2022-06-20 10:48:43 +0100

    Configured postgresql database

--- blogging/settings/development.py
+++ blogging/settings/development.py
@@ -1,1 +1,16 @@
 from .base import *
+
+
+# Database
+# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": env("DB_NAME"),
+        "USER": env("DB_USERNAME"),
+        "HOST": env("DB_HOST"),
+        "PASSWORD": env("DB_PASSWORD"),
+        "PORT": env("DB_PORT"),
+    }
+}
